c1240deb0842c46b466f786361f44c8c4f1d86db
Lovnish Verma <princelv84@gmail.com>
2025-07-08 01:44:47 +0530

    flask using get not secure

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/sum', methods=['GET', 'POST'])
+def sum():
+    # num1 = int(request.form.get('a'))
+    num1 = int(request.args.get('a'))
+    num2 = int(request.args.get('b'))
+    result = num1 + num2
+    return render_template('index.html', result=result)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
